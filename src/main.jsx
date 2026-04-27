0b5275779be70720aa68819d32c37ee75c5920e1
Glen Kelley <glen.t.kelley@gmail.com>
2026-04-28 06:21:23 +1000

    Made sub pages load on deployed version

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from './App.jsx'
 import { AdminScreen } from './components/AdminScreen.jsx'
 import { DebugScreen } from './components/DebugScreen.jsx'
 
-const path = window.location.pathname
-const isAdmin = path.endsWith('/admin')
-const isDebug = path.endsWith('/debug')
+const hash = window.location.hash
+const isAdmin = hash === '#admin'
+const isDebug = hash === '#debug'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
